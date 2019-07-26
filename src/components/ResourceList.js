d1658a4f1e604aa6a22cdd19e1c8fbf7166bb37d
TsimpDim <dimitristsimpitas@gmail.com>
2019-07-26 11:35:00 +0300

    Proper card layout

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Card, Icon, List, Tag } from 'antd'
 import axios from 'axios'
+import Masonry from 'react-masonry-component'
+import './styles/_shared.scss'
 
 const { Meta } = Card;
-
+const masonryOptions = {
+    transitionDuration: 0,
+    gutter: 70,
+};
 
 
 export default class ResourceList extends React.Component {
@@ -39,54 +44,47 @@ export default class ResourceList extends React.Component {
     };
     
     reformatDate = (dateStr) => {
-    let dateArr = dateStr.split("-");  // ex input "2010-01-18"
-    return dateArr[2]+ "/" +dateArr[1]+ "/" +dateArr[0].substring(2); //ex out: "18/01/10"
+        let dateArr = dateStr.split("-");  // ex input "2010-01-18"
+        return dateArr[2]+ "/" +dateArr[1]+ "/" +dateArr[0].substring(2); //ex out: "18/01/10"
+    }
+
+    renderCards = () => {
+        return this.state.resources.map(item => (
+            <Card
+            style={{ width: 300, marginTop: 16 }}
+            actions={[<Icon type="edit" />, <Icon type="link" />, <Icon type="ellipsis" />]}
+            hoverable={true}
+            extra={<span><Icon type="calendar"/> {this.reformatDate(item.date_of_creation)}</span>}
+            >
+                <Meta
+                title={item.title}
+                description={item.note}
+                />
+            
+            {item.tags && item.tags.map(item => (
+                <Tag key={item} style={{marginTop:"10px"}}>{item}</Tag>
+            ))}
+
+            </Card>
+        ));
     }
 
     render () {
         return (
-            <div>
+            <div style={{marginTop:"5em"}}>
 
             {
                 this.state.loading
             ?
 
-                <div><Icon id="loading-spinner" type="loading" spin /></div>
+                <Icon id="loading-spinner" type="loading" spin />
             :
 
-                <List
-                grid={{   
-                    gutter: 0,
-                    xs: 1,
-                    sm: 2,
-                    md: 3,
-                    lg: 4,
-                    xl: 4,
-                    xxl: 5,
-                }}
-                dataSource={this.state.resources}
-                renderItem={item => (
-                    <List.Item>
-                        <Card
-                        style={{ width: 300, marginTop: 16 }}
-                        actions={[<Icon type="edit" />, <Icon type="link" />, <Icon type="ellipsis" />]}
-                        hoverable={true}
-                        extra={<span><Icon type="calendar"/> {this.reformatDate(item.date_of_creation)}</span>}
-                        >
-                            <Meta
-                            title={item.title}
-                            description={item.note}
-                            />
-                        
-                        {item.tags && item.tags.map(item => (
-                            <Tag key={item} style={{marginTop:"10px"}}>{item}</Tag>
-                        ))}
-
-
-                        </Card>
-                    </List.Item>
-                )}
-                />
+                <Masonry
+                options={masonryOptions}
+                >
+                    {this.renderCards()}
+                </Masonry>
             }
         
         
